f19452a045f1d72c799cce550d14e690fe4e29a5
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-12-10 19:10:37 +0200

    three commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import dotenv from 'dotenv';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+dotenv.config();
+const PORT = Number(process.env.PORT) || 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(contactsRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
