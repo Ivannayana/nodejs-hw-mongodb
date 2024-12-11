e3495fee6b5e9974a4e76415d7465569981c6265
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-12-11 19:39:10 +0200

    going hw

---
+++ src/services/contacts.js
@@ -0,0 +1,40 @@
+import { ContactCollection } from '../db/models/Contact.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactCollection.find();
+  return contacts;
+};
+
+export const getContactsById = async (contactId) => {
+  const contact = await ContactCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
