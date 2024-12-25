ad5f684bf4804cf1b2a0503428d328a942c5632e
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-12-25 20:39:37 +0200

    required2

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -25,9 +25,11 @@ export const createContactSchema = Joi.object({
     })
     .messages({
       'string.base': 'Input correct email, for example: example@domain.com',
+      'any.required': 'Email is required',
     }),
   isFavourite: Joi.boolean().messages({
     'boolean.base': 'Field must be true or false',
+    'any.required': 'Field is required',
   }),
   contactType: Joi.string()
     .valid('personal', 'home', 'work')
@@ -36,4 +38,35 @@ export const createContactSchema = Joi.object({
       'any.only': 'Field should have one of this values: personal, home, work',
       'any.required': 'Field is required',
     }),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Name should be a string',
+    'string.min': 'Name should have at least 3 characters',
+    'string.max': 'Name should have at most 20 characters',
+  }),
+  phoneNumber: Joi.string()
+    .pattern(/^[+]?[0-9]{10,15}$/)
+    .min(3)
+    .max(20)
+    .messages({
+      'string.base': 'PhoneNumber should start with "+"',
+      'string.min': 'Name should have at least 3 characters',
+      'string.max': 'Name should have at most 20 characters',
+    }),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .messages({
+      'string.base': 'Input correct email, for example: example@domain.com',
+    }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'Field must be true or false',
+  }),
+  contactType: Joi.string().valid('personal', 'home', 'work').messages({
+    'any.only': 'Field should have one of this values: personal, home, work.',
+  }),
 });
